3eb8e7cc4c05c64e81a609bddefe117219698601
ShinySaphire <isons@onid.oregonstate.edu>
2015-02-09 23:23:17 -0800

    fixed date and time added another option to menu

--- cli_start.py
+++ cli_start.py
@@ -9,25 +9,31 @@ db = MySQLdb.connect("mysql.eecs.oregonstate.edu", "cs419-g6", "group6data", "cs
 cursor = db.cursor() 
 screen.addstr("Welcome to the Simplified Advising Scheduling System\n")
 while True:
-	screen.addstr("Please Select from the following menu:\n1)Read your advising appointments\n2)Manage your advising appointments\n3)Exit\n")
+	screen.addstr("Please Select from the following menu:\n1)Read your advising appointments\n2)Delete a advising appointments\n3)Create Appointment\n4) Exit\n")
 	event = screen.getch()
-	if event == ord("3"): break
+	if event == ord("4"): break
 	elif event == ord("1"):
 		screen.addstr("Please give your name.\n")
 		name = screen.getstr()
 		name2 = "'" + name + "'"
 		screen.addstr("Schedule for " + name + "\n")
 		sql_read = "SELECT * FROM (SELECT a_name, s_name FROM advising_schedule, advisor, student WHERE advising_schedule.ad_id = advisor.a_id AND advising_schedule.stud_id = student.s_id) AS alias WHERE alias.a_name = " + name2 + "\n"
-		screen.addstr("SQL query is: " + sql_read)
+		#screen.addstr("SQL query is: " + sql_read)
 		try:
 			cursor.execute(sql_read)
 			results = cursor.fetchall()
 			for row in results:
-				aname = row[0]
-				sname = row[1]
-				screen.addstr("Appointment with student: " + sname + "\n")
+				appdate = row[0]
+			apptime = row[1]
+			aname = row[2]
+			sname = row[3]
+			newdate = str(appdate)
+			newtime = str(apptime)
+			screen.addstr("Appointment with student: " + sname + " On: " + newdate + " At: " + newtime +"\n")
 		except:
 			screen.addstr("No advisor by that name.\n")
 	elif event == ord("2"):
 		screen.addstr("Please give your name and the name of the student.\n")
+	elif event - ord("3"):
+		screen.addstr("Please give your name and the name of the student, and date and time.\n")
 curses.endwin()
