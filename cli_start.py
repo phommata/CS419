9ef5125ba457fbe21cb9db51c3112d545cde7363
Stephanie Ison <isons@kec1130-16.engr.oregonstate.edu>
2015-02-10 11:45:26 -0800

    broke something, not sure how but the query must not be working right.

--- cli_start.py
+++ cli_start.py
@@ -9,17 +9,18 @@ db = MySQLdb.connect("mysql.eecs.oregonstate.edu", "cs419-g6", "group6data", "cs
 cursor = db.cursor() 
 screen.addstr("Welcome to the Simplified Advising Scheduling System\n")
 while True:
-	screen.addstr("Please Select from the following menu:\n1)Read your advising appointments\n2)Delete a advising appointments\n3)Create Appointment\n4) Exit\n")
+	screen.addstr("\nPlease Select from the following menu:\n1)View Appointments\n2)Delete Appointment\n3)Create Appointment\n4)Exit\n")
 	event = screen.getch()
 	if event == ord("4"): break # 4 = They want to exit CLI client
 	elif event == ord("1"): # 1 They want to read their advising schedule
-		screen.addstr("Please give your name.\n") #Need their name to look up their appointments
+		screen.addstr("\nPlease give your name.\n") #Need their name to look up their appointments
 		name = screen.getstr() #get the string they typed
 		name2 = "'" + name + "'" #force name into a string so it works with the SQL query
 		
-		sql_read = "SELECT * FROM (SELECT a_name, s_name FROM advising_schedule, advisor, student WHERE advising_schedule.ad_id = advisor.a_id AND advising_schedule.stud_id = student.s_id) AS alias WHERE alias.a_name = " + name2 + "\n"
-		#screen.addstr("SQL query is: " + sql_read) #Debugging purposes checking that SQL query appeared the way it should
+		sql_read = "SELECT * FROM (SELECT data, time, a_name, s_name FROM advising_schedule, advisor, student WHERE advising_schedule.ad_id = advisor.a_id AND advising_schedule.stud_id = student.s_id) AS alias WHERE alias.a_name = " + name2 + "\n"
+		screen.addstr("SQL query is: " + sql_read) #Debugging purposes checking that SQL query appeared the way it should
 		try:
+			screen.addstr("Schedule for " + name + "\n")
 			cursor.execute(sql_read)
 			results = cursor.fetchall()
 			for row in results:
@@ -27,9 +28,8 @@ while True:
 				apptime = row[1]
 				aname = row[2]
 				sname = row[3]
-				newdate = str(appdate) #must change from datatime object to string to be able to out put
+				newdate = str(appdate) #must change from object to string
 				newtime = str(apptime) #same as above
-				screen.addstr("Schedule for " + aname + "\n")
 				screen.addstr("Appointment with student: " + sname + " On: " + newdate + " At: " + newtime +"\n")
 		except:
 			screen.addstr("No Advisor named: " + name + "\n")
