b349afa3fa48be6f7487982180df438cbe32bd20
Stephanie Ison <isons@kec1130-18.engr.oregonstate.edu>
2015-02-10 14:51:43 -0800

    no more errors second loop through, changed it to ask for advisor name in the beginning so they only have to give it once.

--- cli_start.py
+++ cli_start.py
@@ -7,19 +7,24 @@ screen = curses.initscr()
 screen.keypad(1) #mode the screen uses to pature key presses.
 db = MySQLdb.connect("mysql.eecs.oregonstate.edu", "cs419-g6", "group6data", "cs419-g6")
 cursor = db.cursor() 
+screen.scrollok(1)
 screen.addstr("Welcome to the Simplified Advising Scheduling System\n")
+screen.addstr("To begin please provide your first and last name.\n") #Need their name in every option, so might as well only ask for it once.
+
+name = screen.getstr() #get the string they typed
+name2 = "'" + name + "'" #force name into a string so it works with the SQL queries
 while True:
-	screen.addstr("\nPlease Select from the following menu:\n1)View Appointments\n2)Delete Appointment\n3)Create Appointment\n4)Exit\n")
+	screen.refresh()
+
+	screen.addstr("\nPlease Select from the following menu:\n1)View Appointments\n2)Create Appointment\n3)Delete Appointment\n4)Exit\n")
 	event = screen.getch()
+	
 	if event == ord("4"): break # 4 = They want to exit CLI client
 	elif event == ord("1"): # 1 They want to read their advising schedule
-		screen.addstr("\nPlease give your name.\n") #Need their name to look up their appointments
-		name = screen.getstr() #get the string they typed
-		name2 = "'" + name + "'" #force name into a string so it works with the SQL query
 		sql_read = "SELECT * FROM (SELECT date, time, a_name, s_name FROM advising_schedule, advisor, student WHERE advising_schedule.ad_id = advisor.a_id AND advising_schedule.stud_id = student.s_id) AS alias WHERE alias.a_name = " + name2 + "\n"
-		screen.addstr("SQL query is: " + sql_read) #Debugging purposes checking that SQL query appeared the way it should
+		#screen.addstr("SQL query is: " + sql_read) #Debugging purposes checking that SQL query appeared the way it should
 		try:
-			screen.addstr("Schedule for " + name + "\n")
+			screen.addstr("\nSchedule for " + name + "\n")
 			cursor.execute(sql_read)
 			results = cursor.fetchall()
 			for row in results:
@@ -32,8 +37,10 @@ while True:
 				screen.addstr("Appointment with student: " + sname + " On: " + newdate + " At: " + newtime +"\n")
 		except:
 			screen.addstr("No Advisor named: " + name + "\n")
-	elif event == ord("2"):
-		screen.addstr("Please give your name and the name of the student.\n")
-	elif event - ord("3"):
-		screen.addstr("Please give your name and the name of the student, and date and time.\n")
+	elif event == ord("2"): #create appointment
+		screen.addstr("\nTo create an appointment please provide:\nThe Student's first and last name\nThe date in the form YYYY-MM-DD\nThe time using 24hour clock.\n")
+	elif event == ord("3"): #delete appointment
+		screen.addstr("\nTo delete an appointment please provide:\nThe Student's first and last name.\nThe date of the appointment\n")
+	
+	#screen.erase()
 curses.endwin()
