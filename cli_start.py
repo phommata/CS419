a52cce3fa2e7b0c7bd8b0303226bff6af1d86441
Stephanie Ison <isons@flip1.engr.oregonstate.edu>
2015-02-10 23:06:21 -0800

    got rid of create appoinment, put in functionality for cancel appointment.

--- cli_start.py
+++ cli_start.py
@@ -15,10 +15,10 @@ name = screen.getstr() #get the string they typed
 name2 = "'" + name + "'" #force name into a string so it works with the SQL queries
 while True:
 	screen.refresh()
-	screen.addstr("\nPlease Select from the following menu:\n1)View Appointments\n2)Create Appointment\n3)Delete Appointment\n4)Exit\n")
+	screen.addstr("\nPlease Select from the following menu:\n1)View Appointments\n2)Cancel Appointment\n3)Exit\n")
 	event = screen.getch()
 	
-	if event == ord("4"): break # 4 = They want to exit CLI client
+	if event == ord("3"): break # 4 = They want to exit CLI client
 	elif event == ord("1"): # 1 They want to read their advising schedule
 		sql_read = "SELECT * FROM (SELECT date, time, a_name, s_name FROM advising_schedule, advisor, student WHERE advising_schedule.ad_id = advisor.a_id AND advising_schedule.stud_id = student.s_id) AS alias WHERE alias.a_name = " + name2 + "\n"
 		#screen.addstr("SQL query is: " + sql_read) #Debugging purposes checking that SQL query appeared the way it should
@@ -35,33 +35,30 @@ while True:
 				newtime = str(apptime) #same as above
 				screen.addstr("Appointment with student: " + sname + " On: " + newdate + " At: " + newtime +"\n")
 		except:
-			screen.addstr("No Advisor named: " + name + "\n")
-	elif event == ord("2"): #create appointment
-		screen.addstr("\nTo create an appointment please provide:\nThe Student's first and last name\n")
-		student_name = screen.getstr()
-		strStudent_name = "'" + student_name + "'"
-		screen.addstr("\nThe date in the form YYYY-MM-DD\n")
-		date = screen.getstr()
-		strDate = "'" + date + "'"
-		screen.addstr("\nThe time using 24hour clock in the format HH:MM:SS.\n")
-		time = screen.getstr()
-		strTime = "'" + time + "'"
+			screen.addstr("No Advisor named: " + name + "\n")	
+	elif event == ord("2"): #Cancel appointment
+		screen.addstr("\nTo cancel an appointment please provide:\nThe Student's first and last name.\n")
+		st_name = screen.getstr()
+		str_st_name = "'" + st_name + "'"
+		screen.addstr("\nThe date of the appointment in the format YYYY-MM-DD\n")
+		can_date = screen.getstr() 
+		str_can_date = str(can_date)
+		sql_emails = "SELECT a_email, s_email FROM advisor, student WHERE advisor.a_name = " + name2 + " AND student.s_name = " + str_st_name + "\n"
 		try:
-			sql_ids = "SELECT a_id, s_id  FROM advisor, student where a_name = " + name2 + "AND s_name = " + strStudent_name + "\n"
-			cursor.execute(sql_ids)
-			ids_result = cursor.fetchall()
-			for row in ids_result:
-				aid = row[0]
-				sid = row[1]
-				str_aid = str(aid)
-				str_sid = str(sid)
-				sql_create = "INSERT INTO advising_schedule (`ad_id`, `stud_id`, `date`, `time`) VALUES (" + str_aid + ", " + str_sid + ", " + strDate + ", " + strTime + ");"
-				cursor.execute(sql_create)
-			screen.addstr("Appointment has been created\n")
+			cursor.execute(sql_emails)
+			emails = cursor.fetchall()
+			for row in emails:
+				ad_email = row[0]
+				stud_email = row[1]
+			screen.addstr("Sending Cancellation Email to " + ad_email + " and " + stud_email + " for " + str_can_date + "\nAre you sure you want to continue? Y/N\n")
+			confirm = screen.getch()
+			if confirm == ord("y") or confirm == ord("Y"):
+				screen.addstr("\nAppointment is Cancelled\n")
+				#Use provided and gathered info to send emails telling student and adviser that appointment is cancelled and procmail takes care of removing it from Outlook and database.
+			elif confirm == ord("n") or confirm == ("N"):
+				screen.addstr("\nAppointment has not been Cancelled\n")
+			else:
+				screen.addstr("\nDidn't Understand your input. Appointment will not be Cancelled\n")
 		except:
-			screen.addstr("Something went wrong.\n")
-	elif event == ord("3"): #delete appointment
-		screen.addstr("\nTo delete an appointment please provide:\nThe Student's first and last name.\nThe date of the appointment\n")
-	
-	#screen.erase()
+			screen.addstr("\nError Please Try Again\n")
 curses.endwin()
